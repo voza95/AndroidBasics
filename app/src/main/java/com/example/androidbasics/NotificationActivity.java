9838921b54aaf8bf1340612baf6b29fde9f6dd65
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2021-02-07 22:21:48 +0530

    Added Notification(II- styles)

--- app/src/main/java/com/example/androidbasics/NotificationActivity.java
+++ app/src/main/java/com/example/androidbasics/NotificationActivity.java
@@ -7,6 +7,9 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,13 +51,27 @@ public class NotificationActivity extends AppCompatActivity {
             Intent broadCastIntent = new Intent(this, NotificationReceiver.class);
             broadCastIntent.putExtra("toastMessage", messageET.getText().toString());
 
+            PendingIntent actionIntent = PendingIntent.getBroadcast(this,0,broadCastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.notification);
+
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                     .setSmallIcon(R.drawable.ic_one)
                     .setContentTitle(titleET.getText().toString())
                     .setContentText(messageET.getText().toString())
+                    .setLargeIcon(largeIcon)
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText(getString(R.string.dummy_txt))
+                            .setBigContentTitle("Big content title")
+                            .setSummaryText("Summary Text")
+                    )
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                    .setContentIntent(contentIntent);
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true)
+                    .setColor(Color.BLUE)
+                    .setContentIntent(contentIntent)
+                    .addAction(R.mipmap.ic_launcher,"Toast",actionIntent);
             Notification notification = builder.build();
 
             //id=1 will over write the same notification.
@@ -66,6 +83,16 @@ public class NotificationActivity extends AppCompatActivity {
                     .setSmallIcon(R.drawable.ic_two)
                     .setContentTitle(titleET.getText().toString())
                     .setContentText(messageET.getText().toString())
+                    .setStyle(new NotificationCompat.InboxStyle()
+                            .addLine("This is Line 1")
+                            .addLine("This is Line 2")
+                            .addLine("This is Line 3")
+                            .addLine("This is Line 4")
+                            .addLine("This is Line 5")
+                            .addLine("This is Line 6")
+                            .setBigContentTitle("Big content title")
+                            .setSummaryText("Summary Text")
+                    )
                     .setPriority(NotificationCompat.PRIORITY_LOW)
                     .setCategory(NotificationCompat.CATEGORY_MESSAGE);
             Notification notification = builder.build();
